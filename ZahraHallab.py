9b29dd168a509025251577e88ac7620d2ab9e4c0
Zahrahall <145912334+Zahrahall@users.noreply.github.com>
2023-09-25 23:14:13 +0300

    Update ZahraHallab.py

--- ZahraHallab.py
+++ ZahraHallab.py
@@ -18,7 +18,7 @@ st.set_page_config(page_title = 'Zahra Dashboard',
                     page_icon = 'bar_chart:',
                     layout = 'wide'
 )
-Create a selectbox for tab selection
+#Create a selectbox for tab selection
 selected_tab = st.sidebar.radio("Select a tab:", ("Main page", ""))
 if selected_tab == "Main page":
     st.header("Average monthly wages for working male and female over the years")
