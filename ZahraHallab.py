e3346e160d00ffa4f6155ad69a79ca8903be8795
Zahrahall <145912334+Zahrahall@users.noreply.github.com>
2023-09-25 22:04:29 +0300

    Update ZahraHallab.py

--- ZahraHallab.py
+++ ZahraHallab.py
@@ -2,20 +2,16 @@ import pandas as pd
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
-import datetime  
-import numpy as np  
+
 from streamlit import components
 # forex
 from forex_python.converter import CurrencyRates
 cr = CurrencyRates()
 #########################################################################################################################
 
-######################################################### LOAD THE MALARIA DATASET #################################################
-#file_url = 'https://github.com/AhmadBakkar/Malaria/blob/master/Malaria.csv'
-#df = pd.read_csv(file_url)
 
-df = pd.read_csv('C:\\Users\\Ahmad\\Desktop\\average_hourly_earnings_of_female_and_male_employees_(managers)_local_currency.csv')
+
+df = pd.read_csv('average_hourly_earnings_of_female_and_male_employees_(managers)_local_currency.csv')
 
 st.set_page_config(page_title = 'Zahra Dashboard',
                     page_icon = 'bar_chart:',
