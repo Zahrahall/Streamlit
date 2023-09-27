9e154e03b418e60fd4c907015a9c08a102ec3fdc
Zahrahall <145912334+Zahrahall@users.noreply.github.com>
2023-09-27 18:17:44 +0300

    Update ZahraHallab.py

--- ZahraHallab.py
+++ ZahraHallab.py
@@ -8,7 +8,7 @@ from streamlit import components
 
 
 
-df = pd.read_csv('average_hourly_earnings_of_female_and_male_employees_(managers)_local_currency1.csv')
+df = pd.read_csv('monthly wages.csv')
 
 
 
@@ -25,7 +25,7 @@ if selected_tab == "Main page":
 
     # You can add more content here, such as charts or images
     st.image("https://th.bing.com/th/id/OIP.6PWkkI1Gr4rgGL62sfH-fgHaE8?w=246&h=180&c=7&r=0&o=5&pid=1.7",use_column_width=True)
-
+    st.write('throughout the years, it is widely known that female monthly wages are less than male monthly wages. what about today?')
 elif selected_tab == "":
     st.header("Charts")
 
@@ -37,8 +37,8 @@ session_state = st.session_state
 
 
 st.subheader("Bar Chart - Monthly wages by Gender")
-bar_fig = px.bar(df, x='year', y='amount_local_currency', color='gender',
-                 labels={'amount_local_currency': 'Amount (Local Currency)'})
+bar_fig = px.scatter(df,x='year',y='amount_adj_usd_currency',color='country',size='amount_adj_usd_currency',hover_name='gender')
+
 st.plotly_chart(bar_fig)
 
 
@@ -46,7 +46,6 @@ st.plotly_chart(bar_fig)
 france_df = df[df['country'] == 'France']
 
 # Line Chart - Trend over Time in France
-st.subheader("Line Chart - Trend over Time in France")
-line_fig = px.line(france_df, x='year', y='amount_local_currency', color='gender',
-                   labels={'amount_local_currency': 'Amount (Local Currency)'})
+st.subheader("")
+line_fig = px.scatter(df,x='year',y='amount_adj_usd_currency',color='gender',marginal_y='histogram',marginal_x='box',trendline='ols',template='plotly')
 st.plotly_chart(line_fig)
