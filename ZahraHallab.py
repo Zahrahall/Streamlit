57f9993d779b0fa894c50e29e2dd492655b52ff4
Zahrahall <145912334+Zahrahall@users.noreply.github.com>
2023-10-11 20:25:59 +0300

    Update ZahraHallab.py

--- ZahraHallab.py
+++ ZahraHallab.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-df = pd.read_csv('monthly_wages.csv')
+df = pd.read_csv('monthly wages.csv')
 
 st.set_page_config(
     page_title='Zahra Dashboard',
