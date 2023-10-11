e78ff1ae21b83a0886b60d5ca5dabec009e99823
Zahrahall <145912334+Zahrahall@users.noreply.github.com>
2023-10-11 20:04:45 +0300

    Update ZahraHallab.py

--- ZahraHallab.py
+++ ZahraHallab.py
@@ -17,25 +17,22 @@ st.set_page_config(page_title = 'Zahra Dashboard',
                     page_icon = 'bar_chart:',
                     layout = 'wide'
 )
-#Create a selectbox for tab selection
-selected_tab = st.sidebar.radio("Select a tab:", ("Main page", "Visualizations"))
-if selected_tab == "Main page":
-    st.header("Average monthly wages for working male and female over the years")
+def front_page():
+  st.header("Average monthly wages for working male and female over the years")
 
     # You can add more content here, such as charts or images
     st.image("https://th.bing.com/th/id/OIP.6PWkkI1Gr4rgGL62sfH-fgHaE8?w=246&h=180&c=7&r=0&o=5&pid=1.7",use_column_width=True)
     st.write('throughout the years, it is widely known that female monthly wages are less than male monthly wages. what about today?')
-elif selected_tab == "Visualizations":
-    st.header("Charts")
+def page_1():
+  st.header("Page 1")
+st.subheader("")
+line =px.scatter(df,x='year',y='amount_adj_usd_currency',color='country',size='amount_adj_usd_currency',hover_name='gender')
 
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-##################################################################################################################################
+st.plotly_chart(line)
 
- # Create a dictionary to store the session state
-session_state = st.session_state
-
-
-st.subheader("Bar Chart - Monthly wages by Gender")
+def page_2():
+    st.header("Page 2")
+  st.subheader("Bar Chart - Monthly wages by Gender")
 # Create a sidebar for filters
 st.sidebar.header("select a country")
 selected_countries = st.sidebar.multiselect("Select Countries:", df['country'].unique())
@@ -53,9 +50,23 @@ bar_fig = px.scatter(filtered_df,x='country',y='amount_adj_usd_currency',color='
 
 # Display the Plotly chart in the separate sidebar
 st.plotly_chart(bar_fig)
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Select a Page", ['Front Page', 'Page 1', 'Page 2'])
+if page == 'Front Page':
+    front_page()
+elif page == 'Page 1':
+    page_1()
 
-# Line Chart - Trend over Time in France
-st.subheader("")
-line =px.scatter(df,x='year',y='amount_adj_usd_currency',color='country',size='amount_adj_usd_currency',hover_name='gender')
+elif page == 'Page 2':
+    page_2()
 
-st.plotly_chart(line)
+ations":
+    st.header("Charts")
+
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+##################################################################################################################################
+
+ # Create a dictionary to store the session state
+session_state = st.session_state
+
+
