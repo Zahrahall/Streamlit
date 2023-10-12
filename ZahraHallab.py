c84878ac1934cbaab0f7850b0adc620847ec9b5a
Zahrahall <145912334+Zahrahall@users.noreply.github.com>
2023-10-12 18:46:58 +0300

    Update ZahraHallab.py

--- ZahraHallab.py
+++ ZahraHallab.py
@@ -31,7 +31,7 @@ def page_1():
         df, x='year', y='amount_adj_usd_currency', color='country', size='amount_adj_usd_currency',
         hover_name='gender', animation_frame='year', animation_group='country',
         log_x=True, range_x=[2010, 2025], range_y=[0, 400]
-          st.plotly_chart(scatter_fig)
+    st.plotly_chart(scatter_fig)
 
     
     if play:
